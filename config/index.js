631c7bd3b580bcdf11f9f0270706cc873bd47200
1622663850 <663809323@qq.com>
2022-09-13 22:56:34 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -127,7 +127,7 @@ export const config = {
     * */
   CUSTOMIZED_DATE_LIST: [
     // 在一起的日子
-    {"keyword": "love_day", date: "2015-05-01"},
+    {"keyword": "love_day", date: "2022-08-17"},
     // 结婚纪念日
     {"keyword": "marry_day", date: "2020-01-04"},
     // 退伍日, 不用可以删掉
