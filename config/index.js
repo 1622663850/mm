4785c40c031d9dbf08fa8f94a11f9218e9cf6c0b
1622663850 <663809323@qq.com>
2022-09-13 22:48:48 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -22,7 +22,7 @@ export const config = {
    *    // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
    *    id: "ohxOO6VskYv0Kuf-ywIZVRMpuXhk",
    *    // 你想对他发送的模板消息的模板ID
-   *    useTemplateId: "mctkpqVK7QWMH2LwlDQOdv-FrOKLXrpo2Q6eoIVGraE	",
+   *    useTemplateId: "mctkpqVK7QWMH2LwlDQOdv-FrOKLXrpo2Q6eoIVGraE",
    *    // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。https://wangxinleo.cn
    *    openUrl: "https://http://xqzyh.top"
    *   }, 
@@ -35,7 +35,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "ogDEC6SiOggBxz_Bs4qXQDFUNWwQ",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "mctkpqVK7QWMH2LwlDQOdv-FrOKLXrpo2Q6eoIVGraE	",
+      useTemplateId: "mctkpqVK7QWMH2LwlDQOdv-FrOKLXrpo2Q6eoIVGraE",
       // 所在省份
       province: "福建",
       // 所在城市
